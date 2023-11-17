5b6a537ba2c65d68d083e7f44c30db0c23b31d07
GdesignHome <graphicdesignhome00@gmail.com>
2023-11-17 19:29:18 +0100

    G

--- script.js
+++ script.js
@@ -69,15 +69,7 @@ const contact2= document.querySelector(".nav-link1");
 
 
 
-const arrow = document.querySelector('.arrow');
 
-arrow.addEventListener('click', function(){
-    window.scrollTo({
-        top:0,
-        behavior:"smooth"
-    })
-   
-})
 
 window.addEventListener("scroll", function(){
     if (window.scrollY>200){
@@ -89,11 +81,52 @@ window.addEventListener("scroll", function(){
 
 // ------------------------------------------------------------
 
+const target = document.getElementById("target");
+// console.log(target)
+let array = ["Inner", "Rythm"];
+let wordIndex = 0;
+let letterIndex = 0;
+
+// target.textContent = array[wordIndex][letterIndex];
+const createLetter = function(){
+    const letter = document.createElement("span");
+    target.appendChild(letter);
+    letter.textContent = array[wordIndex][letterIndex];
+
+    setTimeout(function() {
+        letter.remove();
+    }, 2800)
+    
+}
 
 
+ const loop = function(){
+    setTimeout(function() {
+        if(wordIndex >= array.length){
+            wordIndex = 0;
+            letterIndex = 0;
+            loop();
+        }
+         else if (letterIndex < array[wordIndex].length){
+            createLetter();
+            letterIndex++;
+            loop();
+
+        } else { 
+            wordIndex++;
+            letterIndex = 0;
+            setTimeout(function() {       
+                loop();
+            }, 2800);
+        }
+
+    },60);
+
+ }
+
+ loop();
 
 
-// Initial call to start the slideshow
 
 
 
